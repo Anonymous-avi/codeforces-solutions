3171e6907b74b27b9496b226785ef644fe8ca76e
Anonymous-avi <clownalphareal11@gmail.com>
2026-01-21 09:07:37 +0530

    CF : 266A - Stones on the table

---
+++ 110A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n;
+    cin>>n;
+    int count=0;
+    while(n){
+        int digit=n%10;
+        if(digit==4 || digit==7){
+            count++;
+        }
+        n=n/10;
+    }
+    if(count==4 || count==7){
+        cout<<"YES";
+    }
+    else
+    cout<<"NO";
+
+    return 0;
+
+}
