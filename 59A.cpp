96ce9204aad25ead3b4d3eb86811ed3b9b0e2a56
Anonymous-avi <clownalphareal11@gmail.com>
2026-02-25 16:47:35 +0530

    codeforces 59A : Word

---
+++ 59A.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s =" ";
+    cin>>s;
+    int LowerCount=0;
+    int UpperCount=0;
+    for(int i=0 ; i<s.size() ; i++){
+        if(islower(s[i])){
+            LowerCount++;
+        }
+        else{
+            UpperCount++;
+        }
+    }
+    if(LowerCount>=UpperCount){
+        for(int i=0 ; i<s.size() ; i++){
+            s[i]=tolower(s[i]);
+        }
+    }
+    else{
+        for(int i=0 ; i<s.size() ; i++){
+            s[i]=toupper(s[i]);
+        }
+    }
+cout<<s;
+return 0;
+}
