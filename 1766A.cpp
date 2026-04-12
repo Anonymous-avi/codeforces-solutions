d4a4aad4127f195f7fc2fdbe56666ad1f045ecf0
Anonymous-avi <clownalphareal11@gmail.com>
2026-04-12 15:00:35 +0530

    CF : Extremely Round : 1766A.cpp

---
+++ 1766A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        string s = to_string(n);
+
+        int digits = s.length();
+
+        int ans = (digits - 1) * 9 + (s[0] - '0');
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
