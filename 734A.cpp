082b27607b42a2c21d1748df217786ccb0c862bd
Anonymous-avi <clownalphareal11@gmail.com>
2026-06-07 08:53:38 +0530

    CF : 46 : 734A.CPP : Anton and Danik

---
+++ 734A.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    string s ;
+    cin>>s;
+    int countA =0;
+    int countD=0;
+    for(int i=0 ; i<s.size() ; i++){
+        if(s[i]=='A'){
+           countA++;
+        }
+        else
+        countD++;
+    }
+      
+    if(countA>countD){
+        cout<<"Anton";
+    }
+    else if(countD>countA){
+        cout<<"Danik";
+    }
+    else
+    cout<<"Friendship";
+
+    return 0;
+
+}
