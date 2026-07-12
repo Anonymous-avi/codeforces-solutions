deae19b889cfc4c8966fe27c88eca3a2c4515fe7
Anonymous-avi <clownalphareal11@gmail.com>
2026-07-12 18:35:33 +0530

    CF : Q 5 : FixYou

---
+++ 1391B.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n ,m;
+        cin>>n>>m;
+        vector<vector<char>>grid(n,vector<char>(m));
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<m ; j++){
+                cin>>grid[i][j];
+            }
+        }
+        int changes=0;
+        for(int j=0 ; j<m-1 ; j++){
+            if(grid[n-1][j]!='R'){
+                changes++;
+            }
+        }
+        for(int i= 0 ;i<n-1 ; i++){
+            if(grid[i][m-1]!='D'){
+                changes++;
+            }
+        }
+        cout<<changes<<endl;
+    }
+    return 0;
+}
