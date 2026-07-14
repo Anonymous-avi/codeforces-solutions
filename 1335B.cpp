89dfb4597587c352f807d4eaa17505a9a9fab39e
Anonymous-avi <clownalphareal11@gmail.com>
2026-07-15 01:44:15 +0530

    CF : ques 13 : substring

---
+++ 1335B.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,a,b;
+        cin>>n>>a>>b;
+        string ans=" ";
+        for(int i=0 ; i<n ; i++){
+            ans=ans+char('a' + (i%b));
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
